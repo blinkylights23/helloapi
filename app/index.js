a7e06a15c2cc58d4777647b5181f56bf4e7b20f7
Paul Smith <paul@blinkylights.org>
2016-12-09 06:13:16 -0500

    Use swagger-routes

--- app/index.js
+++ app/index.js
@@ -5,20 +5,28 @@ var express = require('express'),
     jwt = require('jsonwebtoken'),
     config = require('config'),
     morgan = require('morgan'),
-    path = require('path');
+    swaggerRoutes = require('swagger-routes');
 
 
 // The Express app
 var app = express();
+app.use(express.static(`${__dirname}/static`));
 app.set('secret', config.get('secret'));
 
 // Middleware
 app.use(morgan(config.get('morganLogs')));
+swaggerRoutes(app, {
+  api: `${__dirname}/swagger/test.yaml`,
+  docsPath: '/api-docs',
+  handlers: `${__dirname}/handlers`,
+  authorizers: `${__dirname}/security`,
+  maintainHeaders: false
+});
 
 // Routers
-var routes = require('./routes');
-app.use('/', routes.normal);
-app.use('/api', routes.api);
+// var routes = require('./routes');
+// app.use('/', routes.normal);
+// app.use('/api', routes.api);
 
 // Listen
 app.listen(config.get('port'), () => {
